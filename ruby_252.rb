f35395eeed96ddfd8685252e48f7991bdf41dab8
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-12-29 11:49:10 +0900

    252

---
+++ ruby_252.rb
@@ -0,0 +1,33 @@
+t1 = true
+t2 = true
+f1 = false
+puts t1 && t2
+puts t1 && f1
+
+t1 = true
+f1 = false
+f2 = false
+puts t1 || f1
+puts f1 || f2
+
+t1 = true
+t2 = true
+f1 = false
+f2 = false
+puts t1 && t2 || f1 && f2
+
+t1 = true
+t2 = true
+f1 = false
+f2 = false
+puts t1 && (t2 || f1) && f2
+
+t1 = true
+f1 = false
+puts !t1
+puts !f1
+
+t1 = true
+f1 = false
+puts t1 && f1
+puts !(t1 && f1)
