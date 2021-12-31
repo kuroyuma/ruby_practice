e5f3885278b0541e5bfcb4e376402e8e72cfc830
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-12-31 21:10:16 +0900

    ruby253

---
+++ ruby253.rb
@@ -0,0 +1,43 @@
+n = 11
+if n > 10
+  puts '10より大きい'
+else
+  puts '10以下'
+end
+
+country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hello'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '???'
+end
+
+
+
+country = 'italy'
+
+greeting =
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hello'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '???'
+end
+
+greeting
+
+point = 7
+day = 1
+if day == 1
+  puts point *= 5
+end
+
+point
+
