479467dbbb062479273b045a67513b6d498edbc0
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-01-23 22:31:35 +0900

    ruby_212~

---
+++ ruby_212~.rb
@@ -0,0 +1,18 @@
+# require(2.12.7)
+#Dateクラスは組み込みライブラリではないので、そのままでは使用できない
+#puts Date.today
+require 'date'
+puts Date.today
+
+puts 123
+print 123
+p 123
+s = "abc\ndef"
+puts s 
+print s
+p s
+
+a = [1, 2, 3]
+puts a
+print a
+p a
