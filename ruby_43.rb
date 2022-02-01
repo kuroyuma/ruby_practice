acb4e11aedf518cd5c393c28185c848f15aa810e
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-02-01 21:43:51 +0900

    ruby_43

--- ruby_43.rb
+++ ruby_43.rb
@@ -25,3 +25,37 @@ numbers.each do |i|
 end
 puts sum
 
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end
+puts sum
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  #sum_valueはブロック内で初めて登場した変数なので、ブロック内でのみ有効
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end
+#ブロックの外に出ると、sum_valueは参照できない
+#puts sum_value
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+  #sumはブロックの外で作成されたので、ブロック内部でも参照可能
+  sum += sum_value
+end
+
+numbers = [1, 2, 3, 4]
+sum = 0
+sum_value = 100
+#ブロックの外にもsum_valueはあるが、ブロック内ではブロック引数のsum_valueが優先される
+numbers.each do |sum_value|
+  sum += sum_value
+end
+puts sum
