d17a780b7bae0d2782c92345728a697e11133a25
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-02-03 21:45:02 +0900

    ruby_44

---
+++ ruby_44.rb
@@ -0,0 +1,11 @@
+# ブロックを使う配列のメソッド
+# map/collect(4.4.1)
+numbers = [1, 2, 3, 4]
+new_numbers = []
+numbers.each { |n| new_numbers << n * 10 }
+puts new_numbers
+
+numbers = [1, 2, 3, 4, 5]
+#ブロックの戻り値が新しい配列の各要素になる
+new_numbers = numbers.map { |n| n * 10 }
+puts new_numbers
