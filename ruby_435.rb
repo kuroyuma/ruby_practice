0d10b6bfc70db7489c845cca4c3af0c73cb39a31
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-02-03 21:35:04 +0900

    ruby_435

--- ruby_435.rb
+++ ruby_435.rb
@@ -1,1 +1,20 @@
 #do...endと{}(4.3.5)
+numbers = [1, 2, 3, 4]
+sum = 0
+#ブロックをあえて改行せずに書く
+numbers.each do |n| sum += n end
+puts sum
+
+numbers = [1, 2, 3, 4]
+sum = 0
+# do...endの代わりに{}を使う
+numbers.each { |n| sum += n }
+puts sum
+
+numbers = [1, 2, 3, 4]
+sum = 0
+# {}でブロックを作り、なおかつ改行を入れる
+numbers.each { |n|
+  sum += n
+}
+puts sum
