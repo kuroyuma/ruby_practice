55d315960001d80dacbf46439350eb7a8b88beaf
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-02-05 20:00:09 +0900

    ruby_44

--- ruby_44.rb
+++ ruby_44.rb
@@ -21,4 +21,42 @@ numbers = [1, 2, 3, 4, 5, 6]
 non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
 puts non_multiples_of_three
 
-# find/detect
+# find/detect(4.4.4)
+numbers = [1, 2, 3, 4,]
+sum = 0
+numbers.each { |n| sum += n }
+puts sum
+
+numbers = [1, 2, 3, 4]
+sum = numbers.inject(0) { |result, n| result + n }
+puts sum
+
+puts ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].inject('Sun') { |result, s| result + s }
+
+# &とシンボルを使ってもっと簡潔に書く(4.4.5)
+puts ['ruby', 'java', 'perl'].map { |s| s.upcase }
+#こう書き換えられる
+puts ['ruby', 'java', 'perl'].map(&:upcase)
+
+puts [1, 2, 3, 4, 5, 6, 7, 8, 9].select { |n| n.odd? }
+#こう書き換えられる
+puts [1, 2, 3, 4, 5, 6, 7, 8, 9].select(&:odd?)
+
+#範囲(Range)
+# ..を使うと5が範囲に含まれる(1以上5以下)
+range = 1..5
+puts range.include?(0)
+puts range.include?(1)
+puts range.include?(4.9)
+puts range.include?(5)
+puts range.include?(6)
+
+# ...を使うと5が範囲に含まれない(1以上5未満)
+range = 1...5
+puts range.include?(0)
+puts range.include?(1)
+puts range.include?(4.9)
+puts range.include?(5)
+puts range.include?(6)
+
+puts (1..5).include?(1)
