63f506b73f4ecf5f8a397bfd3674b6cb59f718eb
kuroyuma <k.1993.yuma.0324@gmail.com>
2022-02-07 20:14:55 +0900

    ruby_44

--- ruby_44.rb
+++ ruby_44.rb
@@ -120,3 +120,20 @@ puts ('a'...'e').to_a
 
 puts ('bad'..'bag').to_a
 puts ('bad'...'bag').to_a
+
+# 繰り返し処理を行う(4.5.5)
+#範囲オブジェクトを配列に変換してから繰り返し処理を行う
+numbers = (1..4).to_a
+sum = 0
+numbers.each { |n| sum += n }
+puts sum
+
+sum = 0
+# 範囲オブジェクトに対して直接eachメソッドを呼び出す
+(1..4).each { |n| sum += n }
+puts sum
+
+numbers = []
+#1から10まで2つ飛ばしで繰り返しを行う
+(1..10).step(2) { |n| numbers << n }
+puts numbers
